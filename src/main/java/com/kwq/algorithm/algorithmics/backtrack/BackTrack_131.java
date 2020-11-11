281655df159cd8b18ef0293bd1f8841b0b4cacc5
kongweiqiang <kongwq12@chinaunicom.cn>
2020-11-11 16:18:27 +0800

    回溯

--- src/main/java/com/kwq/algorithm/algorithmics/backtrack/BackTrack_131.java
+++ src/main/java/com/kwq/algorithm/algorithmics/backtrack/BackTrack_131.java
@@ -6,18 +6,33 @@ import java.util.List;
 /**
  * @auther : kongweiqiang
  * @DATE : 2020/11/6
- * @DESC :
+ * @DESC : 分割回文串
  */
 public class BackTrack_131 {
-
 
     public List<List<String>> partition(String s) {
         List<String> item = new ArrayList<>();
         List<List<String>> result = new ArrayList<>();
         if(s == null) return null;
         if(s.length() == 0) return result;
+        back(s,0,item,result);
+        return result;
+    }
 
-        return null;
+    private void back(String s,int index,List<String> item,List<List<String>> result){
+        if(index == s.length()){
+            result.add(new ArrayList<>(item));
+            return;
+        }
+        for(int i = index;i<s.length();i++){
+            if(sTs(s.substring(index,i + 1))){
+                item.add(s.substring(index,i + 1));
+            }else{
+                continue;
+            }
+            back(s,i + 1,item,result);
+            item.remove(item.size() - 1);
+        }
     }
 
     //双指针法
@@ -35,7 +50,8 @@ public class BackTrack_131 {
 
     public static void main(String[] args) {
         BackTrack_131 back = new BackTrack_131();
-        boolean flag = back.sTs("abcTcba");
-        System.out.println(flag);
+        //boolean flag = back.sTs("abcTcba");
+        List<List<String>> result = back.partition("aab");
+        System.out.println(result);
     }
 }
